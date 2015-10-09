1f5dcb7457c05fa1d2b9d4f53658320392df90a1
Haruka Togawa <haruka.moyuk@gmail.com>
2015-10-09 11:08:59 +0900

    Add Xcode project

---
+++ PLNInformationViewController/PLNInformationViewController.h
@@ -0,0 +1,13 @@
+//
+//  PLNInformationViewController.h
+//  PLNInformationViewController
+//
+//  Created by Haruka Togawa on 10/9/15.
+//  Copyright © 2015 Haruka Togawa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PLNInformationViewController : NSObject
+
+@end
---
+++ PLNInformationViewController/PLNInformationViewController.m
@@ -0,0 +1,13 @@
+//
+//  PLNInformationViewController.m
+//  PLNInformationViewController
+//
+//  Created by Haruka Togawa on 10/9/15.
+//  Copyright © 2015 Haruka Togawa. All rights reserved.
+//
+
+#import "PLNInformationViewController.h"
+
+@implementation PLNInformationViewController
+
+@end
